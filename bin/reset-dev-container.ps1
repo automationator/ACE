04c317090709712eba6558f4b62b9a9c8446f74d
John Davison <unixfreak0037@gmail.com>
2020-07-07 23:10:45 -0400

    make reset-dev-container also work on windows

--- bin/reset-dev-container.ps1
+++ bin/reset-dev-container.ps1
@@ -1,9 +1,24 @@
 docker-compose -f .\docker-compose-dev.yml stop
-docker container rm ace-dev > /dev/null 2>&1
-docker container rm ace-db-dev > /dev/null 2>&1
-docker volume rm ace-data-dev > /dev/null 2>&1
-docker volume rm ace-db-dev > /dev/null 2>&1
+docker container rm ace-dev > $null 2>&1
+docker container rm ace-db-dev > $null 2>&1
+docker volume rm ace-data-dev > $null 2>&1
+docker volume rm ace-db-dev > $null 2>&1
 & .\bin\build-docker-images.ps1
 docker-compose -f .\docker-compose-dev.yml up -d
 docker exec -it -u root ace-dev /bin/bash -c 'chown -R ace:ace /opt/ace/data'
 docker exec -it -u root ace-dev /bin/bash -c 'docker/provision/ace/install -r'
+
+Write-Host -NoNewLine "waiting for database..."
+while ($true) {
+    docker exec -it -u ace ace-dev /bin/bash -it -c 'ace test-database-connections' > $null 2>&1
+    if ($?) {
+        Write-Host ""
+        break
+    }
+
+    Write-Host -NoNewLine "."
+    Start-Sleep -s 1
+}
+
+docker exec -it -u ace ace-dev /bin/bash -it -c 'ace enc set --password=ace'
+docker exec -it -u ace ace-dev /bin/bash -it -c 'ace user add --password=analyst analyst analyst@localhost'
